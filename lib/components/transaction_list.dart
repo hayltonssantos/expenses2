7ef7e1cb98a8a723a22f67c3b6177ecc4a7033a2
Haylton Santos <aylton.souza10@gmail.com>
2025-09-15 13:48:07 +0100

    146. Usamos o layout builder para podermos dimensionar os containers a partir da altura defnida pelo tamanho da tela

---
+++ lib/components/transaction_list.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import '../models/transaction.dart';
+import 'package:intl/intl.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transaction;
+  final void Function(String) onRemove;
+
+  const TransactionList(this.transaction, this.onRemove, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return transaction.isEmpty
+        ? LayoutBuilder( //bom para fazermos algo caber na tela
+          builder: (ctx, constrains){
+            return Column(
+            children: <Widget>[
+              SizedBox(height: constrains.maxHeight * 0.05), // espaçamento
+              Container(
+                height: constrains.maxHeight * 0.3,
+                child: Text(
+                  'Nenhuma Transação Cadastrada!',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+              ),
+              SizedBox(height: constrains.maxHeight * 0.05), // espaçamento
+              SizedBox(
+                height: constrains.maxHeight * 0.6,
+                child: Image.asset(
+                  'assets/images/waiting.png',
+                  fit: BoxFit.cover,
+                ),
+              )
+            ],
+          );
+          })
+        : ListView.builder(
+            itemCount: transaction.length, // tamanho da lista
+            itemBuilder: (ctx, index) {
+              final tr = transaction[index];
+              return Card(
+                elevation: 5,
+                margin: EdgeInsets.symmetric(
+                  vertical: 8,
+                  horizontal: 5,
+                ),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 30,
+                    child: Padding(
+                      padding: const EdgeInsets.all(6),
+                      child: FittedBox(
+                        child: Text('R\$${tr.value}'),
+                      ),
+                    ),
+                  ),
+                  title: Text(
+                    tr.title,
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                  subtitle: Text(
+                    DateFormat('d MMM y').format(tr.date),
+                  ),
+                  trailing: IconButton(
+                    icon: Icon(
+                      Icons.remove_circle_outlined,
+                      color: Colors.red,
+                    ),
+                    onPressed: () => onRemove(tr.id),
+                  ),
+                ),
+              );
+            },
+          );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,149 @@
+import 'package:expenses/components/chart.dart';
+import 'package:expenses/components/transaction_form.dart';
+import 'package:flutter/material.dart';
+import 'dart:math';
+import 'components/transaction_list.dart';
+import 'models/transaction.dart';
+ 
+main() => runApp(ExpensesApp());
+ 
+class ExpensesApp extends StatelessWidget {
+  ExpensesApp({super.key});
+  final ThemeData tema = ThemeData();
+ 
+  @override
+  Widget build(BuildContext context) {
+/*     SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp
+    ]); //Seta o modo fixo em retrato caso seja necessario  */
+    return MaterialApp(
+      home: const MyHomePage(),
+      theme: ThemeData(
+        useMaterial3: false,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.purple,
+          foregroundColor: Colors.white,
+        ),
+        textTheme: ThemeData.light().textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 20 * MediaQuery.textScalerOf(context).scale(1),
+            fontWeight: FontWeight.bold
+          )
+        ),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.amber,
+          primary: Colors.purple,
+          secondary: Colors.amber,
+        ),
+        fontFamily: 'Quicksand',
+      ),
+    );
+  }
+}
+ 
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+ 
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+ 
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _transactions = [  ];
+  bool _showChart = false;
+
+  List<Transaction> get _recentTransactions{
+    return _transactions.where((tr){
+      return tr.date.isAfter(DateTime.now().subtract(Duration(days:7)));
+    }).toList();
+  }
+ 
+  _addTransaction(String title, double value, DateTime date) {
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: date,
+    );
+ 
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+ 
+    Navigator.of(context).pop();
+  }
+
+  _removeTransaction(String id){
+    setState(() {
+      _transactions.removeWhere((tr) => tr.id == id);
+    });
+  }
+ 
+  _openTransactionFormModal(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (_) {
+        return TransactionForm(_addTransaction);
+      },
+    );
+  }
+ 
+  @override
+  Widget build(BuildContext context) {
+    
+    final appBar = AppBar(
+        title: const Text('Despesas Pessoais'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () => _openTransactionFormModal(context),
+          ),
+        ],
+      );
+    
+    final availableHeight = MediaQuery.of(context).size.height 
+    - appBar.preferredSize.height - MediaQuery.of(context).padding.top;
+
+    return Scaffold(
+      appBar: appBar,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Row(
+              mainAxisAlignment:  MainAxisAlignment.center,
+              children: [
+                Text('Exibir Gráfico'),
+                Switch(
+                  value: _showChart, 
+                  onChanged: (value){
+                   setState(() {
+                     _showChart = value;
+                   });
+                }),
+              ],
+            ),
+            if (_showChart)(
+              SizedBox(
+                height: availableHeight * 0.3,
+                child: Chart(_recentTransactions),
+                )
+              ),
+            if (!_showChart)(
+              SizedBox(
+                height: availableHeight * 0.7,
+                child: TransactionList(_transactions, _removeTransaction)
+                )
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () => _openTransactionFormModal(context),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
